9c14e42179db6877a8ae6159c994a510c89edbe6
MikiKru <michal_kruczkowski@o2.pl>
2020-07-12 10:38:38 +0200

    jadv -d4 -v6

--- src/main/java/lambda_stream_optional/StreamApiWithModel.java
+++ src/main/java/lambda_stream_optional/StreamApiWithModel.java
@@ -1,8 +1,12 @@
 package lambda_stream_optional;
 
 import oop.model.User;
+import oop.model.enums.Gender;
+import oop.model.enums.Role;
 
 import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class StreamApiWithModel {
     // metoda wypisująca zawartość listy users z InMemoryData
@@ -21,11 +25,24 @@ public class StreamApiWithModel {
                 .anyMatch(user -> user.getEmail().equals(email) && user.getPassword().equals(password) &&
                         user.isStatus() && !user.isRemoved());
     }
-
+    // metoda zwracająca liczbę kobiet
+    public long countAllWomen(){
+        return InMemoryData.users.stream()
+                .filter(user -> user.getGender().equals(Gender.WOMAN))
+                .count();
+    }
+    // metoda zwracająca listę użytkowników zawierającą rolę podaną w argumencie metody
+    public List<User> getAllUsersContainsRole(Role role){
+        return InMemoryData.users.stream()
+                .filter(user -> user.getRoles().contains(role))
+                .collect(Collectors.toList());
+    }
     public static void main(String[] args) {
         StreamApiWithModel sapi = new StreamApiWithModel();
 //        sapi.getAllUsers();
         sapi.getAllUsersOrderByRegistrationDateDesc();
-        System.out.println("Wynik logowania: " + sapi.loginUser("hl@hl.pl","hl"));
+//        System.out.println("Wynik logowania: " + sapi.loginUser("hl@hl.pl","hl"));
+        System.out.println("Liczba kobiet: " + sapi.countAllWomen());
+        sapi.getAllUsersContainsRole(Role.ROLE_ADMIN).forEach(System.out::println);
     }
 }
