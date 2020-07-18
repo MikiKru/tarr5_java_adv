58fcb16460ad9039d63a33bf81b6c5838d092425
MikiKru <michal_kruczkowski@o2.pl>
2020-07-18 13:41:33 +0200

    jadv -d5 -v15

--- src/main/java/threads/ThreadController.java
+++ src/main/java/threads/ThreadController.java
@@ -1,24 +1,22 @@
 package threads;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class ThreadController {
-    List<Integer> numbers = new ArrayList<>(Arrays.asList(2,3,1,4,3,4,5,3));
+    Deque<Integer> numbers = new ArrayDeque<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
 
     // metoda implementująca wątek wypisujący wszystkie liczby
     public void printNumbers(){
-        Runnable target;
         // utworzenie klasy anonimowej
         Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
-                for (Integer number : numbers) {
+                while(!numbers.isEmpty()) {
                     try {
-                        Thread.currentThread().sleep(1000);     // uśpienie wąktu thread na 1s
-                        System.out.println("Wątek: " + Thread.currentThread().getName());
-                        System.out.println(number);
+                        Thread.currentThread().sleep(new Random().nextInt(6) * 1000);     // uśpienie wąktu thread na 1s
+                        System.out.println("Wątek: " + Thread.currentThread().getName() +
+                                " - wartość: " + numbers.removeFirst() +
+                                " aktualna zawartość: " + numbers);
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     } } }
@@ -28,6 +26,8 @@ public class ThreadController {
     public static void main(String[] args) {
         ThreadController tc = new ThreadController();
         System.out.println("Wątek: " + Thread.currentThread().getName());
+        tc.printNumbers();
+        tc.printNumbers();
         tc.printNumbers();
         System.out.println("Wątek: " +Thread.currentThread().getName());
 
