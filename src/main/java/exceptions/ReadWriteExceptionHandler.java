09222871575bbe24583b38acd51bb0cba32f0e3b
MikiKru <michal_kruczkowski@o2.pl>
2020-07-18 12:03:30 +0200

    jadv -d5 -v11

--- src/main/java/exceptions/ReadWriteExceptionHandler.java
+++ src/main/java/exceptions/ReadWriteExceptionHandler.java
@@ -1,6 +1,7 @@
 package exceptions;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Paths;
@@ -33,14 +34,24 @@ public class ReadWriteExceptionHandler {
             } catch (InputMismatchException | NumberFormatException e) {
                 e.printStackTrace();
                 System.out.println("Błąd typu danych");
-            } finally {
+            } finally {     // klauzula ta wykonuje się zawsze po bloku try lub dowolnym bloku catch
                 try {
                     fileWriter.close();
                 } catch (IOException e) {
                     e.printStackTrace();
                 } } } }
-    public static void readDataFromFile(){ }
+    public static void readDataFromFile(){
+        try {
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()){
+                System.out.println(scanner.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
     public static void main(String[] args) {
          appendDataToFile();
+         readDataFromFile();
     }
 }
