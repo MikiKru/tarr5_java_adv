d211f54f5fc2378f88e4236ea74c795834a5e5aa
MikiKru <michal_kruczkowski@o2.pl>
2020-07-19 09:27:01 +0200

    jadv -d5 -v19

--- src/main/java/threadsAccount/Client.java
+++ src/main/java/threadsAccount/Client.java
@@ -11,14 +11,21 @@ public class Client implements Runnable {
     private Account account;
     @Override
     public void run() {
-        while(account.getAccountSaldo() > 0){
+        int i = 10;
+        while(i >= 0){      // pętla wykona się i razy
             try {
                 Thread.sleep(new Random().nextInt(new Random().nextInt(1000)));
-                int amount = new Random().nextInt((10) + 1) * 10;
-                account.getOutcome(amount);
+                int amount = new Random().nextInt((10) + 1) * 10;       // kwota wpłaty lub wypłaty z konta
+                int decision = new Random().nextInt(2);            // zmienna decydująca o wpłacie lub wypłacie
+                if(decision == 0) {
+                    account.getOutcome(amount);                         // wypłata z konta
+                } else {
+                    account.addIncome(amount);                          // wpłata na konto
+                }
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+            i --;
         }
     }
 }
